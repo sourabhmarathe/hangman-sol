fb145312747f5172bf774f6c0cc597da958ec056
Sourabh Marathe <surja795@gmail.com>
2021-12-18 16:51:19 -0500

    Change tests to Hangman contract

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Hangman = await hre.ethers.getContractFactory("Hangman");
+  const hangman = await Hangman.deploy("Hello, Hardhat!"); //TODO: Need the right deploy params
 
-  await greeter.deployed();
+  await hangman.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Hangman deployed to:", hangman.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
