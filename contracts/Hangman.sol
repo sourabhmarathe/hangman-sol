994842cdd23991d156001391573ede8204effa15
Sourabh Marathe <surja795@gmail.com>
2021-12-18 17:26:37 -0500

    Fix a bunch of bugs

--- contracts/Hangman.sol
+++ contracts/Hangman.sol
@@ -13,35 +13,43 @@ contract Hangman {
         secret = _secret;
         guesses = "";
         lives = _lives;
+        bytes memory solutionBytes = bytes(secret);
         for (uint i = 0; i < bytes(secret).length; i++) {
-            abi.encodePacked(solution, "_");
+            solutionBytes[i] = "_";
         }
+        solution = string(solutionBytes);
     }
 
     event Outcome(string message);
     event CorrectGuess(string guessesMade, string solution);
     event IncorrectGuess(uint livesRemaining);
 
-    // Helper function that returns true if {what} is a substring of {where}
-    function contains(string memory what, string memory where) pure internal returns (bool) {
-        bytes memory whatBytes = bytes(what);
-        bytes memory whereBytes = bytes(where);
+    // Helper function that returns true if {where} is a substring of {what}
+    function contains (string memory what, string memory where) pure internal returns (bool) {
+        bytes memory whatBytes = bytes (what);
+        bytes memory whereBytes = bytes (where);
 
+        bool found = false;
         for (uint i = 0; i < whereBytes.length - whatBytes.length; i++) {
-            for (uint j = 0; j < whatBytes.length; j++) {
-                if (whereBytes [i + j] == whatBytes [j]) {
-                    return true;
+            bool flag = true;
+            for (uint j = 0; j < whatBytes.length; j++)
+                if (whereBytes [i + j] != whatBytes [j]) {
+                    flag = false;
+                    break;
                 }
+            if (flag) {
+                found = true;
+                break;
             }
         }
-        return false;
+        return found;
     }
 
     function secretSolved() internal view returns (bool) {
         bytes memory solutionBytes = bytes(solution);
         bytes memory secretBytes = bytes(secret);
-        for (uint i = 0; i < secretBytes.length; i++) {
-            if (solutionBytes[i] != secretBytes[i]) {
+        for(uint i = 0; i < secretBytes.length; i++) {
+            if(solutionBytes[i] != secretBytes[i]) {
                 return false;
             }
         }
@@ -49,16 +57,21 @@ contract Hangman {
     }
 
     function makeGuess(string memory _guess) external {
-        require(bytes(_guess).length == 1);
-        require(!contains(_guess, guesses));
+        console.log(_guess);
+        require(bytes(_guess).length == 1, "you may only guess one character");
+        require(contains(guesses, _guess), "you already guessed that");
+        console.log("hi");
 
         // Append to the current set of guesses
         string(abi.encodePacked(guesses, _guess));
+        console.log(guesses);
 
         // Check if the guess is correct
-        if (!contains(secret, _guess)) {
+        if(!contains(_guess, secret)) {
+            console.log("wrong guess, minus one life");
+            console.log(secret);
             lives = lives - 1;
-            if (lives == 0) {
+            if(lives == 0) {
                 emit Outcome("Guesser has lost");
                 return;
             }
@@ -70,8 +83,8 @@ contract Hangman {
         bytes memory solutionBytes = bytes(solution);
         bytes memory secretBytes = bytes(secret);
         bytes memory guessByte = bytes(_guess);
-        for (uint256 i = 0; i < solutionBytes.length; i++) {
-            if (guessByte[0] == secretBytes[i]) {
+        for(uint256 i = 0; i < solutionBytes.length; i++) {
+            if(guessByte[0] == secretBytes[i]) {
                 solutionBytes[i] = guessByte[0];
             }
         }
--- test/sample-test.js
+++ test/sample-test.js
@@ -4,16 +4,24 @@ const { ethers } = require("hardhat");
 describe("Hangman", function () {
   it("Should create and execute a simple game of hangman", async function () {
     const Hangman = await ethers.getContractFactory("Hangman");
-    const hangman = await Hangman.deploy("")
+    const hangman = await Hangman.deploy("abc", 2);
     await hangman.deployed();
 
     expect(await hangman.solution()).to.equal("___");
 
-    //const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const makeGuessTx = await hangman.makeGuess("a");
 
     // wait until the transaction is mined
-    // await setGreetingTx.wait();
+    await makeGuessTx.wait();
 
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await hangman.solution()).to.equal("a__");
+
+    const makeGuessTx2 = await hangman.makeGuess("b");
+    await makeGuessTx2.wait();
+
+    const makeGuessTx3 = await hangman.makeGuess("c");
+    await makeGuessTx3.wait();
+
+    expect(await hangman.solution()).to.equal("abc");
   });
 });
